76bf1965bdb62f9ffc329fd1a7943ff4463e7892
SpencerTower <spencerjtower1@gmail.com>
2021-10-26 16:04:20 -0700

    adds comment out for console.log messages

--- js/app.js
+++ js/app.js
@@ -25,66 +25,66 @@ function guessingGame() {
     
     let cityResponse = prompt("Is Spencer from Seattle?").toLowerCase();
     if ((cityResponse === 'yes') || (cityResponse === 'y')) {
-        console.log("cityResponse: " + "Correct response.");
+        //console.log("cityResponse: " + "Correct response.");
         alert("That's right!");
         userScore += 1;
     } else if ((cityResponse === 'no') || (cityResponse === 'n')) {
-        console.log("cityResponse: " + "Incorrect response.");
+        //console.log("cityResponse: " + "Incorrect response.");
         alert("Incorrect! Spencer is from Seattle.");
     } else {
-        console.log("cityResponse: " + "Invalid response.");
+        //console.log("cityResponse: " + "Invalid response.");
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
     let favSportResponse = prompt("Is Spencer's favorite sport basketball?").toLowerCase();
     if ((favSportResponse === 'yes') || (favSportResponse === 'y')) {
-        console.log("favSportResponse: " + "Incorrect response.");
+        //console.log("favSportResponse: " + "Incorrect response.");
         alert("Nope, it's soccer!");
     } else if ((favSportResponse === 'no') || (favSportResponse === 'n')) {
-        console.log("favSportResponse: " + "Correct response.");
+        //console.log("favSportResponse: " + "Correct response.");
         alert("Correct! It's soccer.");
         userScore += 1;
     } else {
-        console.log("favSportResponse: " + "Invalid response.");
+        //console.log("favSportResponse: " + "Invalid response.");
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
     let meditateResponse = prompt("Does Spencer like to meditate?").toLowerCase();
     if ((meditateResponse === 'yes') || (meditateResponse === 'y')) {
-        console.log("meditateResponse: " + "Correct response.");
+        //console.log("meditateResponse: " + "Correct response.");
         alert("Correct! Spencer likes to meditate everyday.");
         userScore += 1;
     } else if ((meditateResponse === 'no') || (meditateResponse === 'n')) {
-        console.log("meditateResponse: " + "Incorrect response.");
+        //console.log("meditateResponse: " + "Incorrect response.");
         alert("Wrong!");
     } else {
-        console.log("meditateResponse: " + "Invalid response.")
+        //console.log("meditateResponse: " + "Invalid response.")
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
     let favBookResponse = prompt("Is Dune Spencer's favorite book?").toLowerCase();
     if ((favBookResponse === 'yes') || favBookResponse === 'y') {
-        console.log("favBookResponse: " + "Correct response.");
+        //console.log("favBookResponse: " + "Correct response.");
         alert("That's correct!");
         userScore += 1;
     } else if ((favBookResponse === 'no') || (favBookResponse === 'n')) {
-        console.log("favBookResponse: " + "Incorrect response.");
+        //console.log("favBookResponse: " + "Incorrect response.");
         alert("Wrong! Dune is the best book ever!");
     } else {
-        console.log("favBookResponse: " + "Invalid response.");
+        //console.log("favBookResponse: " + "Invalid response.");
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
     let hikingResponse = prompt("Does Spencer like to go hiking?").toLowerCase();
     if ((hikingResponse === 'yes') || (hikingResponse === 'y')) {
-        console.log("hikingResponse: " + "Correct response.");
+        //console.log("hikingResponse: " + "Correct response.");
         alert("That's right! Especially Mailbox Peak.");
         userScore += 1;
     } else if ((hikingResponse === 'no') || (hikingResponse === 'n')) {
-        console.log("hikingResponse: " + "Incorrect response.");
+        //console.log("hikingResponse: " + "Incorrect response.");
         alert("Incorrect!");
     } else {
-        console.log("hikingResponse: " + "Invalid response.");
+        //console.log("hikingResponse: " + "Invalid response.");
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
