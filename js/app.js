4fbaf8159d37ab4d87c1bd5f86d683d7ae8727ef
SpencerTower <spencerjtower1@gmail.com>
2021-10-27 14:43:38 -0700

    adds question 7

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ function guessingGame() {
     
     let userScore = 0;
     
+    // question 1
     let cityResponse = prompt("Is Spencer from Seattle?").toLowerCase();
     if ((cityResponse === 'yes') || (cityResponse === 'y')) {
         //console.log("cityResponse: " + "Correct response.");
@@ -36,6 +37,7 @@ function guessingGame() {
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
+    // question 2
     let favSportResponse = prompt("Is Spencer's favorite sport basketball?").toLowerCase();
     if ((favSportResponse === 'yes') || (favSportResponse === 'y')) {
         //console.log("favSportResponse: " + "Incorrect response.");
@@ -49,6 +51,7 @@ function guessingGame() {
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
+    // question 3
     let meditateResponse = prompt("Does Spencer like to meditate?").toLowerCase();
     if ((meditateResponse === 'yes') || (meditateResponse === 'y')) {
         //console.log("meditateResponse: " + "Correct response.");
@@ -62,6 +65,7 @@ function guessingGame() {
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
+    // question 4
     let favBookResponse = prompt("Is Dune Spencer's favorite book?").toLowerCase();
     if ((favBookResponse === 'yes') || favBookResponse === 'y') {
         //console.log("favBookResponse: " + "Correct response.");
@@ -75,6 +79,7 @@ function guessingGame() {
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
+    // question 5
     let hikingResponse = prompt("Does Spencer like to go hiking?").toLowerCase();
     if ((hikingResponse === 'yes') || (hikingResponse === 'y')) {
         //console.log("hikingResponse: " + "Correct response.");
@@ -88,23 +93,59 @@ function guessingGame() {
         alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
     }
 
-            
+    // question 6
+    // guess a number
+    for (let i=0; i <=4; i++) {
+        let numberGuess = prompt("Guess a number between 1 and 100:");
+        if (numberGuess < 74) {
+            alert("Too low! Guess again.");
+        } else if (numberGuess > 74) {
+            alert("Too high! Guess again.");
+        // loose equivalent to accept string
+        } else if (numberGuess == 74) {
+            alert("You guessed it!");
+            userScore += 1;
+            break;
+        } else {
+            alert("Please enter a number between 1 and 100:");
+        }
+    }
 
+    // question 7
+    placesVisited = ['england', 'thailand', 'eduador', 'japan', 'canada', 'spain'];
+    for (let i = 0; i <= 6; i++) {
+        let placesResponse = prompt("What countries have I visited?").toLowerCase(); 
+        for (let i = 0; i <= placesVisited.length(); i++) {
+            if (placesResponse === placesVisited[i]) {
+                alert("Correct!");
+                break;
+            } 
+        }
+    }
 
-    alert("Well done " + userName + " you got " + userScore + "/5 questions right!");
-} 
+}
 */
-// guess a number
-for (let i=0; i <=4; i++) {
-    let numberGuess = prompt("Guess a number between 1 and 100:");
-    if (numberGuess < 74) {
-        alert("Too low! Guess again.");
-    } else if (numberGuess > 74) {
-        alert("Too high! Guess again.");
-    } else if (numberGuess == 74) {
-        alert("You guessed it!");
+
+// question 7
+let placesVisited = ['england', 'thailand', 'ecuador', 'japan', 'canada', 'spain'];
+let answer = false;
+for (let i = 6; i >= 6; i--) {
+    let placesResponse = prompt("What countries have I visited?").toLowerCase(); 
+    for (let i = 0; i <= placesVisited.length; i++) {
+        if (placesResponse === placesVisited[i]) {
+            alert("Correct!");
+            answer = true;
+            userScore += 1;
+            break;
+        }
+    
+    if (answer === true) {
         break;
     } else {
-        alert("Please enter a number between 1 and 100:");
+        alert("Guess again!");
     }
     }
+    if (i === 0) {
+        alert("Your out of guesses!")
+    }
+}
