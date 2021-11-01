dabfadd61f3e4aabcff6cebf9b6f4409f8532349
SpencerTower <spencerjtower1@gmail.com>
2021-10-31 20:37:08 -0700

    refactors questions into functions

--- js/app.js
+++ js/app.js
@@ -5,23 +5,21 @@
 let userName = prompt("Hi, I'm Spencer. What is your name?");
 alert("Hello, " + userName + ". Welcome to the site!");
 
+alert("Click the 'Start' button to initiate a guessing game.")
 
-let userResponse = prompt("Would you like to play a guessing game to find out more about me?").toLowerCase();
+//let userResponse = prompt("Would you like to play a guessing game to find out more about me?").toLowerCase();
 
 // if respond yes, run guessing game
 if ((userResponse === 'yes') || (userResponse === 'y')) {
     alert("Great, let's begin!");
-    guessingGame();
-} else if ((userResponse === 'no') || (userResponse === 'n')){
+} else if ((userResponse === 'no') || (userResponse === 'n')) {
     alert("Goodbye!");
 } else {
     alert("Invalid input. Goodbye!");
 }
 
-
-function guessingGame() {
-    
-    
+// questions 1-5
+function aboutMeGuess() {
     let userScore = 0;
     // Outer most layer for our question array
     let questionsArray = [
@@ -30,104 +28,31 @@ function guessingGame() {
         //rightAnwer
         ["yes", "no", "yes", "yes", "yes"],
         //wrongAnswer
-        ["no", "yes", "no", "no", "no"],  
-        //responseCorrect
-        // ["correct", "correct", "correct", "correct", "correct"],
-        // // responseIncorrect
-        // []    
+        ["no", "yes", "no", "no", "no"]
     ];
 
-
-    for(let x = 0; x < questionsArray[0].length; x += 1) {
+    for (let x = 0; x < questionsArray[0].length; x += 1) {
         // Inside this loop, we will prompt the user a question based off index 0 of questionsArray.
         let userAnswer = prompt(questionsArray[0][x]); // index 1
-        let answerNormalized = userAnswer.toLowerCase();
+        userAnswer = userAnswer.toLowerCase();
 
-        if(answerNormalized === questionsArray[1][x]) {
-            alert('congrat');
+        if (userAnswer === questionsArray[1][x]) {
+            alert('Correct!');
             userScore += 1;
-        } else if (answerNormalized === questionsArray[2][x]) {
-            alert('you suck');
+        } else if (userAnswer === questionsArray[2][x]) {
+            alert('Incorrect!');
         } else {
             alert("That is not a valid answer, please enter 'yes' or 'no'");
             x -= 1;
         }
     }
-/*
-    // question 1
-    let cityResponse = prompt("Am I from Seattle?").toLowerCase();
-    if ((cityResponse === 'yes') || (cityResponse === 'y')) {
-        //console.log("cityResponse: " + "Correct response.");
-        alert("That's right!");
-        userScore += 1;
-    } else if ((cityResponse === 'no') || (cityResponse === 'n')) {
-        //console.log("cityResponse: " + "Incorrect response.");
-        alert("Incorrect! I am from Seattle.");
-    } else {
-        //console.log("cityResponse: " + "Invalid response.");
-        alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
-    }
+}
 
-    // question 2
-    let favSportResponse = prompt("Is my favorite sport basketball?").toLowerCase();
-    if ((favSportResponse === 'yes') || (favSportResponse === 'y')) {
-        //console.log("favSportResponse: " + "Incorrect response.");
-        alert("Nope, it's soccer!");
-    } else if ((favSportResponse === 'no') || (favSportResponse === 'n')) {
-        //console.log("favSportResponse: " + "Correct response.");
-        alert("Correct! It's soccer.");
-        userScore += 1;
-    } else {
-        //console.log("favSportResponse: " + "Invalid response.");
-        alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
-    }
-
-    // question 3
-    let meditateResponse = prompt("Do I like to meditate?").toLowerCase();
-    if ((meditateResponse === 'yes') || (meditateResponse === 'y')) {
-        //console.log("meditateResponse: " + "Correct response.");
-        alert("Correct! I like to meditate everyday.");
-        userScore += 1;
-    } else if ((meditateResponse === 'no') || (meditateResponse === 'n')) {
-        //console.log("meditateResponse: " + "Incorrect response.");
-        alert("Wrong!");
-    } else {
-        //console.log("meditateResponse: " + "Invalid response.")
-        alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
-    }
-
-    // question 4
-    let favBookResponse = prompt("Is Dune my favorite book?").toLowerCase();
-    if ((favBookResponse === 'yes') || favBookResponse === 'y') {
-        //console.log("favBookResponse: " + "Correct response.");
-        alert("That's correct!");
-        userScore += 1;
-    } else if ((favBookResponse === 'no') || (favBookResponse === 'n')) {
-        //console.log("favBookResponse: " + "Incorrect response.");
-        alert("Wrong! Dune is the best book ever!");
-    } else {
-        //console.log("favBookResponse: " + "Invalid response.");
-        alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
-    }
-
-    // question 5
-    let hikingResponse = prompt("Do I like to go hiking?").toLowerCase();
-    if ((hikingResponse === 'yes') || (hikingResponse === 'y')) {
-        //console.log("hikingResponse: " + "Correct response.");
-        alert("That's right! Especially Mailbox Peak.");
-        userScore += 1;
-    } else if ((hikingResponse === 'no') || (hikingResponse === 'n')) {
-        //console.log("hikingResponse: " + "Incorrect response.");
-        alert("Incorrect!");
-    } else {
-        //console.log("hikingResponse: " + "Invalid response.");
-        alert("Invalid response. Please answer 'yes'/'y' or 'no'/'n'.")
-    }
-
-    // question 6
+// question 6
+function numberGuess() {
     let correctNum = 74;
     let guessLimit = 4;
-    for (let i=0; i < guessLimit; i++) {
+    for (let i = 0; i < guessLimit; i++) {
         let numberGuess = parseInt(prompt("Guess a number between 1 and 100:"));
         console.log("numberGuess: " + numberGuess);
 
@@ -145,8 +70,10 @@ function guessingGame() {
             alert("Please enter a number between 1 and 100:");
         }
     }
+}
 
-    // question 7
+// question 7
+function placesVisitedGuess() {
     let placesVisited = ['england', 'thailand', 'ecuador', 'japan', 'canada', 'spain'];
     let answer = false;
     for (let i = 0; i < 6; i++) {
@@ -162,21 +89,20 @@ function guessingGame() {
                 break;
             }
         }
-
-        // check if got right answer
+        // check if right answer
         if (answer === true) {
             break;
         } else {
             alert("Nope, guess again!");
         }
-
-        // if (i === 0) {
-        //     alert("Your out of guesses!")
-        // }
-        
     }
-*/
-    // tell user score
-    alert("Well done, you got " + userScore + "/7 questions correct!");
-    
 }
+
+function initiateGuessingGame() {
+    aboutMeGuess();
+    numberGuess();
+    placesVisitedGuess();
+}
+
+// tell user their score
+alert("Well done, you got " + userScore + "/7 questions correct!");
